52bdd9bc99ef497f6bce4ec5960586455ddce442
sam cocquyt <samcocquyt04@gmail.com>
2026-03-15 23:21:44 -0400

    play_round function added

--- Lab9_xenzonz-1.py
+++ Lab9_xenzonz-1.py
@@ -20,18 +20,40 @@ def get_play_choice() -> str:
         if choice in ("y", "n"):
             return choice
 
-def play_round():
-    return 0 #toss coins
+def play_round(player1: Player, player2: Player) -> None:
+    
+    #if both coins match, player1 wins a coin, player2 loses a coin
+    #if no coins match, player1 loses a coin, player2 wins a coin
+
+    player1.toss_coin()
+    player2.toss_coin()
+
+    side_player1: str = player1.get_coin_side()
+    side_player2: str = player2.get_coin_side()
+
+    print(f"{player1.get_name()} tossed {side_player1}")
+    print(f"{player2.get_name()} tossed {side_player2}")
+
+    if side_player1 == side_player2:
+        player1.win_coin() #win
+        player2.lose_coin() #lose
+        print(f"matched, {player1.get_name()} wins a coin")
+    else:
+        player1.lose_coin() #lose
+        player2.win_coin() #win
+        print(f"no match, {player2.get_name()} wins a coin")
+
+
 
 def print_wallets(player1: Player, player2: Player) -> None:
-    
+
     #player 1
     print(f"{player1.get_name()} has {player1.get_wallet()} coins") 
 
     #player 2
     print(f"{player2.get_name()} has {player2.get_wallet()} coins") 
 
-def final_score_results():
+def final_score_results(player1: Player, player2: Player):
     return 0 #final score and announce winner 
 
 def main() -> None:
