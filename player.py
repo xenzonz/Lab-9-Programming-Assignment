551f13fe1fc81134c8bc53225cda32b2912b7994
sam cocquyt <samcocquyt04@gmail.com>
2026-03-15 22:27:13 -0400

    player class initialized and functions added

--- player.py
+++ player.py
@@ -9,6 +9,29 @@ iv. No starter code
 v. 3/15/2026
 """
 
+from coin import Coin
+
 class Player:
-    def __init__(self):
-        pass
+    def __init__(self, name: str) -> None:
+
+        self.__name: str = name
+        self.__wallet: int = 20
+        self.__coin: Coin = Coin()
+
+    def toss_coin():
+        return 0
+
+    def get_coin_side():
+        return 0
+    
+    def lose_coin():
+        return 0
+    
+    def get_wallet():
+        return 0
+    
+    def get_name():
+        return 0
+
+
+
