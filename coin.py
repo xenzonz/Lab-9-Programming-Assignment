1d003bb989a14a8f38ece33674289ceea0595f2f
sam cocquyt <samcocquyt04@gmail.com>
2026-03-15 22:11:36 -0400

    coin class code added

--- coin.py
+++ coin.py
@@ -9,6 +9,21 @@ iv. No starter code
 v. 3/15/2026
 """
 
+import random
+
 class Coin:
-    def __init__(self):
-        pass
+    
+    def __init__(self) -> None:
+        #initialize coin object, head side up by default
+        self.__sideup: str = "heads"
+
+    def toss(self):
+        if random.randint(0,1) == 0:
+            self.__sideup = "heads"
+        else:
+            self.__sideup = "tails"
+
+    def get_sideup(self):
+        return self.__sideup
+
+print(Coin)
